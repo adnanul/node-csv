602d275156b96ae298b5c4a55ed2402e88b4bfda
Feroj Bepari <feroj21@gmail.com>
2014-09-15 16:46:49 +0600

    First commit in this project

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 node-csv
 ========
 
-A simple csv read/write module for Nodejs.
+A simple csv read/write module for Nodejs. This is our first Nodejs project to deploy as npm by Feroj and Dip
---
+++ app.js
@@ -0,0 +1,15 @@
+"use strict"
+var fs = require('fs');
+
+
+
+
+var myfile = "myfile.txt";
+
+if(fs.existsSync(myfile)) {
+    var content = fs.readFileSync(myfile, 'utf8');
+    fs.writeFileSync(myfile, content.toUpperCase());
+    console.log("Done");
+} else {
+    console.log("File does not exist - " + myfile);
+}
